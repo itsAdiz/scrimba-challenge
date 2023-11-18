9df49ec4285b7491077347bdc7c039b334b4b032
Adi'Z <93094643+itsAdiz@users.noreply.github.com>
2023-11-18 22:47:01 +0500

    Forgot to save the code lol

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let homeScore = 0 ;
 let guestScore = 0 ;
-
+let homeEl = document.getElementsByClassName("homescore")[0];
+let guestEl = document.getElementsByClassName("guestscore")[0];
 
 function addScoreToHome(toAdd){
     homeScore += toAdd;
-    document.getElementsByClassName("homescore").innerHTML = homeScore;
+    homeEl.innerText = homeScore;
+}
+function addScoreToGuest(toAdd){
+    guestScore += toAdd;
+    guestEl.innerText = guestScore;
 }
